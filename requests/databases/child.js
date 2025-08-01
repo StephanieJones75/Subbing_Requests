938e2ed62da8e13174a6d41393ccbe63ef99b748
StephanieJones75 <originalsteph@live.com>
2025-07-31 18:30:49 -0600

    Delete requests/databases/child.js
    Moved to another folder

--- requests/databases/child.js
+++
@@ -1,8 +0,0 @@
-class Child {
-    constructor(id, name, colour_team) {
-        this.id = id
-        this.name = name;
-        this.colour_team = colour_team;
-    }
-}
-const child = new Child();
